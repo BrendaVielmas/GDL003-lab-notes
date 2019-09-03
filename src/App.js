8c50b6b06ab0a9fa3f908d7c2c6799a5bb3a7819
Brenda Vielmas <breen.vielmas@gmail.com>
2019-09-03 18:48:15 -0500

    prueba de funciones

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './index.css';
+import Greet from "./components/Greet"
+import Welcome from "./components/Welcome"
+import * as serviceWorker from './serviceWorker';
+import Tittle from "./components/Tittle"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Comment {
+    render () {
+        return (
+            <div className="App">
+            {/* <Greet /> */ }
+            {/*<Welcome /> */}
+            <Hello/>
+            </div>
+        );
+    }
 }
 
 export default App;
