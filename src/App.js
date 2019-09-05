a431492f69bc2e9706874eafa012c8f1f3f7bdd7
Brenda Vielmas <breen.vielmas@gmail.com>
2019-09-05 18:38:44 -0500

    Login con Google

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import React, {Component} from 'react';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase';
-import Auth from './Auth';
+import './App.css';
+//import * as serviceWorker from './serviceWorker';
+//import * as firebase from 'firebase';
+//import Auth from './Auth';
+import Login from "./Login";
 
 
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  //firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
-	constructor () {
-		super ()
-		this.state = {nota: "cargando nota"}
-	}
-	componentDidMount() {
-		const docRef = firebase.firestore().collection("prueba").doc("c8JSPplRHnnYqUREqbqm").get().then((doc)=>{
-			this.setState({
-				nota: doc.data().nota
-			})
-		})
-	}
+	// constructor () {
+	// 	super ()
+	// 	this.state = {nota: "cargando nota"}
+	// }
+	// componentDidMount() {
+	// 	const docRef = firebase.firestore().collection("prueba").doc("c8JSPplRHnnYqUREqbqm").get().then((doc)=>{
+	// 		this.setState({
+	// 			nota: doc.data().nota
+	// 		})
+	// 	})
+	// }
     render () {
-
         return (
             <div className="App">
-            	<p>{this.state.nota}</p>
-            	<Auth></Auth>
+            	{/* <p>{this.state.nota}</p> */}
+               <Login />
             </div>
-            
+                       
         );
     }
 }
-
 export default App;
